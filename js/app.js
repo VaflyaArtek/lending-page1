2b642cb232c9edf36e9411d084a570a087389634
unknown <yuriy.shpak.2017@gmail.com>
2023-06-21 02:05:09 +0300

    web

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-window.addEventListener('load', function () {
-    var loader = document.getElementById('loader');
-    var content = document.getElementById('content');
+// window.addEventListener('load', function () {
+//     var loader = document.getElementById('loader');
+//     var content = document.getElementById('content');
 
-    setTimeout(function () {
-        loader.style.display = 'none';
-        content.style.display = 'block';
-    }, 5500);
-});
+//     setTimeout(function () {
+//         loader.style.display = 'none';
+//         content.style.display = 'block';
+//     }, 5500);
+// });
