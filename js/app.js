46c346685922f9dcdeca32a9ea9c625c99f89eaf
VaflyaArtek <yuriy.shpak.2017@gmail.com>
2023-10-16 14:47:48 +0300

    drakosha

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
-window.addEventListener('load', function () {
-    var loader = document.getElementById('loader');
-    var content = document.getElementById('content');
+window.addEventListener("load", function () {
+  const loader = document.getElementById("loader");
+  const content = document.getElementById("content");
 
-    setTimeout(function () {
-        loader.style.display = 'none';
-        content.style.display = 'block';
-    }, 1500);
+  setTimeout(function () {
+    loader.style.display = "none";
+    content.style.display = "block";
+  }, 1500);
+});
+
+const contract = document.querySelector(".roadmap__contract");
+const dataValue = contract.getAttribute("data-value");
+
+contract.addEventListener("click", () => {
+  const textArea = document.createElement("textarea");
+  textArea.value = dataValue;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textArea);
+  alert("Value copied: " + dataValue);
 });
